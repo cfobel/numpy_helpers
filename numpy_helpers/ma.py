7d46b70f0201417a8abd2debed7780834d710897
Christian Fobel <christian@fobel.net>
2013-08-11 08:15:51 -0400

    Add masked_as_lists function
    Add `masked_as_lists` function to return a list of strings representing
    formatted rows of a masked array, denoting masked positions using
    dashes.

--- numpy_helpers/ma.py
+++ numpy_helpers/ma.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def pformat(masked_array):
+def masked_as_lists(masked_array):
     rank = len(masked_array.shape)
 
     slices = tuple([slice(0, masked_array.shape[i]) for i in xrange(rank - 1)])
@@ -9,11 +9,16 @@ def pformat(masked_array):
     digits = int(np.ceil(np.log10(masked_array.max())))
 
     f = '%%%dd' % digits
+
+    masked_lists = [', '.join([f % block_id if not m else digits * '-'
+                               for block_id, m in zip(row, row.mask)])
+                    for row in masked_array[slices]]
+    return masked_lists
+
+
+def pformat(masked_array):
+    rank = len(masked_array.shape)
+    masked_lists = masked_as_lists(masked_array)
     line = ']\n' + ((rank - 1) * ' ') + '['
 
-    formatted_masked = line.join([', '.join([f % block_id
-                                             if not m else digits * '-'
-                                             for block_id, m in zip(row,
-                                                                    row.mask)])
-                                  for row in masked_array[slices]])
-    return rank * '[' + formatted_masked + rank * ']'
+    return rank * '[' + line.join(masked_lists) + rank * ']'
